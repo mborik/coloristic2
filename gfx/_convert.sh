1ae6766dea1d48de1ac027608e10ed022014178a
mborik128 <mborik@users.sourceforge.net>
2023-09-27 22:26:49 +0200

    well done dialog gfx

--- gfx/_convert.sh
+++ gfx/_convert.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+gfx2next -bitmap-y -pal-none -pal-file=base.pal dialog.png
+
+tail -c +16384 dialog.nxi > dialog.nxi.tmp
+rm -f dialog.nxi
+head -c 49152 dialog.nxi.tmp > dialog.nxi
+rm -f dialog.nxi.tmp
+
 gfx2next -bitmap-y -pal-none -pal-file=base.pal menu.png
 
 tail -c +16384 menu.nxi > menu.nxi.tmp
